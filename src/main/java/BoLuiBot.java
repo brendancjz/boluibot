5f46eb81acddb650711a1d4e512c47bc335f0dba
Brendan Chia <brendancheerful@gmail.com>
2021-07-20 23:49:34 +0800

    make it better

---
+++ src/main/java/BoLuiBot.java
@@ -0,0 +1,189 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+
+public class BoLuiBot extends TelegramLongPollingBot {
+
+    private boolean isInputtingEntry = false;
+    private String typeOfEntry = "";
+    private ArrayList<String> entryList = new ArrayList<>();
+    private ArrayList<ArrayList<String>> entriesList = new ArrayList<>();
+    private int currEventState = 1;
+
+    @Override
+    public String getBotUsername() {
+        return "bo_lui_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1763203814:AAFbCkdniUC5EJpiLMb3Uq5GUIP7xj60Mpw";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+
+            String text = update.getMessage().getText();
+            boolean cancelCondition = text.length() >= 7 && text.substring(0, 7).equals("/cancel");
+
+            //Text is a command and isInputtingEntry is false.
+            if (text.charAt(0) == '/' && !isInputtingEntry) {
+                if (text.equals("/start")) {
+                    generateStartEvent(update, message);
+                } else if (text.equals("/entries")) {
+                    generateEntriesEvent(message);
+                } else {
+                    generateEventStateOne(text, message);
+                }
+
+
+            } else if (isInputtingEntry && currEventState == 2) {
+                if (cancelCondition) { //User cancels entry.
+                    cancelEntry(message);
+                } else {
+                    generateEventStateTwo(text, message);
+                }
+            } else if (isInputtingEntry && currEventState == 3) {
+                if (cancelCondition) { //User cancels entry.
+                    cancelEntry(message);
+                } else {
+                    generateEventStateThree(text, message);
+                }
+            } else if (isInputtingEntry && currEventState == 4) {
+                if (cancelCondition) { //User cancels entry.
+                    cancelEntry(message);
+                } else {
+                    generateEventStateFour(text, message);
+                    System.out.println(entriesList.toString());
+                }
+
+            } else {
+                message.setText(text); //Echo text
+            }
+
+
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void generateStartEvent(Update update, SendMessage message) {
+        String intro = "";
+        intro += "Hi " + update.getMessage().getChat().getFirstName() +
+                "! I am Bo Lui and I welcome you to Sir Brendan's financial tracker to track how deep your pockets are! Sir Brendan is my creator.\n\n";
+        intro += "For now, I am in the beta stages and so, I have very limited functionalities. I may crash on you. I probably will crash on you... " +
+                "But! Your opinion and feedback to the creator will surely improve my system, so thank you for using me! \n\n";
+        intro += "Enter: \"/\" to see what I can do...";
+
+        message.setText(intro);
+    }
+
+    public void generateEntriesEvent(SendMessage message) {
+        String entries = "ENTRIES\n";
+        entries += "|---------------- \n";
+        for (ArrayList<String> entry : entriesList) {
+
+            if (entry.get(0) == "spend") {
+                entries += "-> SPENT | " + entry.get(2) + " | " + entry.get(1) + " | " + entry.get(3) + "\n";
+            } else if (entry.get(0) == "earn") {
+                entries += "-> EARN | " + entry.get(2) + " | " + entry.get(1) + " | " + entry.get(3) + "\n";
+            }
+
+            entries += "|---------------- \n";
+        }
+
+        if (entriesList.isEmpty()) {
+            message.setText("No Entries. Let's add one.");
+        } else {
+            message.setText(entries);
+        }
+
+    }
+    public void generateEventStateOne(String command, SendMessage message) {
+        if (command.equals("/spend")) {
+            message.setText("Alright, what did you spend on? [Input Category]");
+            isInputtingEntry = true;
+            typeOfEntry = "spend";
+
+        } else if (command.equals("/earn")) {
+            message.setText("Swee! Where did you earn this moolah? [Input Category]");
+            isInputtingEntry = true;
+            typeOfEntry = "earn";
+        }
+
+        entryList.add(typeOfEntry);
+        currEventState++; //Update Event State
+    }
+
+    public void generateEventStateTwo(String text, SendMessage message) {
+        if (typeOfEntry.equals("spend")) {
+            entryList.add(text); //Getting the category
+            message.setText("Okay, how much did you spend on " + text + "? [Input Cost]");
+        } else if (typeOfEntry.equals("earn")) {
+            entryList.add(text); //Getting the category
+            message.setText("Okay, how much did you earn from " + text + "? [Input Earnings]");
+        } else {
+            message.setText("Uh oh.. Something broke.");
+        }
+
+        currEventState++; //Update Event State
+    }
+
+    public void generateEventStateThree(String text, SendMessage message) {
+        if (typeOfEntry.equals("spend")) {
+            entryList.add(text); //Getting the cost
+            message.setText("$" + text + ", got it. Now, what's the story behind this? [Input Description]");
+        } else if (typeOfEntry.equals("earn")) {
+            entryList.add(text); //Getting the earnings
+            message.setText("$" + text +"! Nice! How do you feel earning $" + text + "? [Input Description]");
+        } else {
+            message.setText("Uh oh.. Something broke.");
+        }
+
+        currEventState++; //Update Event State
+    }
+
+    public void generateEventStateFour(String text, SendMessage message) {
+        if (typeOfEntry.equals("spend")) {
+            entryList.add(text); //Getting the description
+            message.setText("Thanks! You have added a new entry: \nSpent $" + entryList.get(2) + " on " + entryList.get(1) + " - \"" + entryList.get(3) + "\"");
+        } else if (typeOfEntry.equals("earn")) {
+            entryList.add(text); //Getting the description
+            message.setText("Thanks! You have added a new entry: \nEarned $" + entryList.get(2) + " from " + entryList.get(1) + " - Feeling: \"" + entryList.get(3) + "\"");
+        } else {
+            message.setText("Uh oh.. Something broke.");
+        }
+
+        updateEntriesList();
+        resetEntry();
+    }
+
+    public void updateEntriesList() {
+        entriesList.add(entryList);
+    }
+
+    public void cancelEntry(SendMessage message) {
+        message.setText("Entry cancelled.");
+        resetEntry();
+    }
+
+    public void resetEntry() {
+        currEventState = 1;
+        isInputtingEntry = false;
+        entryList = new ArrayList<String>();
+    }
+}
+
+
