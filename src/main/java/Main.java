95eea0c50aa5eb2227a77b1b9ba6ab7142dd40b7
Brendan Chia <brendancheerful@gmail.com>
2021-07-21 22:06:38 +0800

    Editing entries insertion\

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,16 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new BoLuiBot());
-        } catch (TelegramApiException e) {
+        } catch (TelegramApiException | URISyntaxException | SQLException e) {
             e.printStackTrace();
         }
 
