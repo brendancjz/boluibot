5a3036ac4934cc6156165692e64c9aa2b921dee1
brendancjz <brendancjz@gmail.com>
2022-06-01 12:05:08 +0800

    fixed .gitignore

--- src/main/java/SummaryBot.java
+++ src/main/java/SummaryBot.java
@@ -12,12 +12,12 @@ public class SummaryBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "bo_lui_bot";
+        return System.getenv("BOT_USERNAME");
     }
 
     @Override
     public String getBotToken() {
-        return "1763203814:AAFbCkdniUC5EJpiLMb3Uq5GUIP7xj60Mpw";
+        return System.getenv("BOT_TOKEN");
     }
 
     @Override
